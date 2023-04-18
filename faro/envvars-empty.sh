5fecbf0e00b1839e49328cb54d5298e0108d92e2
davidryder <david.ryder@grafana.com>
2023-04-18 08:50:19 -0700

    initial

--- faro/envvars-empty.sh
+++ faro/envvars-empty.sh
@@ -1,2 +1,5 @@
-# Grafana Faro Application Key
+# Grafana Faro Configuration
+export GRAFANA_FARO_APP_NAME="Grafana Server"
+export GRAFANA_FARO_APP_VERSION="1.0.0"
+export GRAFANA_FARO_APP_ENVIRONMENT="production"
 export GRAFANA_FARO_APP_KEY="<REQUIRED>
