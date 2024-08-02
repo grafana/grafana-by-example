95e5968a19a64bce741444e08ed5970ec9d5af53
davidryder <david.ryder@grafana.com>
2024-08-02 08:58:24 -0700

    Initial

---
+++ pseudonymization/empty-envvars.sh
@@ -0,0 +1,8 @@
+# Grafana Cloud OTLP End Points and Authorization
+# Used by otel-config.yaml
+export GRAFANA_CLOUD_OTLP_INSTANCE_ID="REQUIRED"
+export GRAFANA_CLOUD_OTLP_PASSWORD="REQUIRED"
+export GRAFANA_CLOUD_OTLP_HTTP_ENDPOINT="https://otlp-gateway-REQUIRED.grafana.net/otlp"
+
+# Grafana Cloud config
+# TBD
