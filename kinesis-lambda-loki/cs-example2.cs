2bcaea445f435702fe175b439bf1db4b959dc30e
daviddryder <89228383+daviddryder@users.noreply.github.com>
2024-09-11 08:09:27 -0700

    Create cs-example2.cs

---
+++ kinesis-lambda-loki/cs-example2.cs
@@ -0,0 +1,32 @@
+// Example showing use of the Serilog.Sinks.Loki Library with Grafana Cloud
+
+using Serilog;
+using Serilog.Sinks.Loki;
+
+
+// Grafana Cloud Credentials
+var lokiHost = Environment.GetEnvironmentVariable("GRAFANA_LOGS_HOST");
+var lokiUser = Environment.GetEnvironmentVariable("GRAFANA_LOGS_USERNAME");
+var lokiPwd = Environment.GetEnvironmentVariable("GRAFANA_LOGS_API_KEY");
+
+Console.WriteLine($"Host: {lokiHost}");
+Console.WriteLine($"User: {lokiUser}");
+
+var credentialsCloud = new BasicAuthCredentials($"https://{lokiHost}", lokiUser, lokiPwd );
+var credentialsLocal = new NoAuthCredentials("http://grafana1:3100");
+
+Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Information()
+        .Enrich.FromLogContext()
+        .WriteTo.LokiHttp(credentialsCloud)
+        .CreateLogger();
+
+Log.Error("Example error message");
+
+var position = new { Latitude = 25, Longitude = 134 };
+var elapsedMs = 34;
+Log.Information("Message processed {@Position} in {Elapsed:000} ms.", position, elapsedMs);
+
+Log.CloseAndFlush();
+
+Console.WriteLine("End");
