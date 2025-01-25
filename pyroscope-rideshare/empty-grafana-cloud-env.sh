1823698159309d84d2cd338042a9276dfed94e73
David Ryder <davidryder@Davids-MacBook-Pro.local>
2025-01-25 13:47:24 -0800

    Initial

---
+++ pyroscope-rideshare/empty-grafana-cloud-env.sh
@@ -0,0 +1,12 @@
+#
+# Apple002
+export OTEL_EXPORTER_OTLP_PROTOCOL="http/protobuf"
+export OTEL_EXPORTER_OTLP_ENDPOINT="https://otlp-gateway-<REQUIRED>.grafana.net/otlp"
+export OTEL_EXPORTER_OTLP_HEADERS="Authorization=Basic <NOT_REQUIRED>"
+export GRAFANA_CLOUD_OTLP_INSTANCE_ID="<REQUIRED>"
+export GRAFANA_CLOUD_OTLP_PASSWORD="<REQUIRED>"
+#
+# Apple002
+export PYROSCOPE_SERVER_ADDRESS="https://profiles-<REQUIRED>.grafana.net"
+export PYROSCOPE_BASIC_AUTH_USER="<REQUIRED>"
+export PYROSCOPE_BASIC_AUTH_PASSWORD="<REQUIRED>"
