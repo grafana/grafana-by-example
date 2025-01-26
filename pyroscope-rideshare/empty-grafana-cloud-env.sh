5672b0cdb9f6b61ec17dfd056173d5f2b6c570bd
David Ryder <davidryder@DavidMacBookPro.local>
2025-01-25 17:13:16 -0800

    Updates

--- pyroscope-rideshare/empty-grafana-cloud-env.sh
+++ pyroscope-rideshare/empty-grafana-cloud-env.sh
@@ -10,3 +10,11 @@ export GRAFANA_CLOUD_OTLP_PASSWORD="<REQUIRED>"
 export PYROSCOPE_SERVER_ADDRESS="https://profiles-<REQUIRED>.grafana.net"
 export PYROSCOPE_BASIC_AUTH_USER="<REQUIRED>"
 export PYROSCOPE_BASIC_AUTH_PASSWORD="<REQUIRED>"
+#
+#
+# Service name
+export OTEL_SERVICE_NAME: rideshare.java.ryder
+# Gafana Cloud Logs
+export GRAFANA_CLOUD_LOGS_ENDPOINT="https://<REQUIRED>.grafana.net/loki/api/v1/push"
+export GRAFANA_CLOUD_LOGS_ID="<REQUIRED>"
+export GRAFANA_CLOUD_LOGS_PASSWORD="<REQUIRED>"
